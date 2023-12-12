1b287bf2284bb62d753ea8b9d66b7fa4f4537453
jymbocala <jym.bocala@gmail.com>
2023-12-12 13:00:05 +1100

    added update and delete routes for tasks

--- src/blueprints/tasks_bp.py
+++ src/blueprints/tasks_bp.py
@@ -6,7 +6,7 @@ from auth import admin_required
 
 tasks_bp = Blueprint('tasks', __name__, url_prefix='/tasks')
 
-# get all tasks
+# Get all tasks
 @tasks_bp.route('/')
 @jwt_required()
 def all_tasks():
@@ -16,7 +16,7 @@ def all_tasks():
     tasks = db.session.scalars(stmt).all()
     return TaskSchema(many=True).dump(tasks)
 
-# get one task
+# Get one task
 @tasks_bp.route('/<int:id>')
 @jwt_required()
 def one_task(id):
@@ -33,5 +33,41 @@ def one_task(id):
 @jwt_required()
 def create_task():
     task_info = TaskSchema(exclude=['id', 'date_created']).load(request.json)
-    print(task_info)
-    return "task created"
+    task = Task(
+        title=task_info['title'],
+        description=task_info['description']
+    )
+
+    db.session.add(task)
+    db.session.commit()
+    return TaskSchema().dump(task), 201
+
+# Update a task
+@tasks_bp.route('/<int:id>', methods=['PUT', 'PATCH'])
+@jwt_required()
+def update_task(id):
+    task_info = TaskSchema(exclude=['id', 'date_created']).load(request.json)
+    stmt = db.select(Task).filter_by(id=id) # .where(Task.id == id)
+    task = db.session.scalar(stmt)
+    if task:
+        task.title = task_info.get('title', task.title)
+        task.description = task_info.get('description', task.description)
+        db.session.commit()
+        return TaskSchema().dump(task)
+    else:
+        return {'error': 'Task not found'}, 404
+    
+# Delete a task
+@tasks_bp.route('/<int:id>', methods=['DELETE'])
+@jwt_required()
+def delete_task(id):
+    admin_required()
+    stmt = db.select(Task).filter_by(id=id) # .where(Task.id == id)
+    task = db.session.scalar(stmt)
+    if task:
+        db.session.delete(task)
+        db.session.commit()
+        return 'Task deleted', 200
+    else:
+        return {'error': 'Task not found'}, 404
+    
